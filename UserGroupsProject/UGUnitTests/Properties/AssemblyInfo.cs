f42b545474c2058810e28043093e98f44bf10421
Witold Skobrtal <witold_sko@op.pl>
2018-08-15 22:57:35 +0200

    fixed problem whith unit test project - renamed

---
+++ UserGroupsProject/UGUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UGUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UGUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74bb4739-53a9-41f6-9db1-5e4717242180")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UserGroupsProject/UGUnitTests/TestGroupRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserGroupsProject;
+using UserGroupsProject.Models;
+
+namespace UserGroupProjectUnitTest
+{
+    class TestGroupRepository : IGroupRepository
+    {
+        public List<Group> GetAll()
+        {
+            List<Group> group = new List<Group>();
+            Group testGroup = new Group();
+            testGroup.Name = "Milk drinkers";
+            testGroup.Id = 1;
+            group.Add(testGroup);
+
+            return group;
+        }
+        public Group Details(int id)
+        {
+
+            Group testGroup = new Group();
+            testGroup.Name = "Sport Fans";
+            testGroup.Id = 1;
+            return testGroup;
+        }
+        public void Delete(Group group, int id)
+        {
+
+        }
+        public Group Edit(Group group, int id)
+        {
+            Group testGroup = new Group();
+            testGroup.Name = "Sport Fans";
+            testGroup.Id = 1;
+            return testGroup;
+
+        }
+
+        public void Create(Group group)
+        {
+
+        }
+        public List<User> GetUserNames(int id)
+        {
+            List<User> user = new List<User>();
+            User testUser = new User();
+            testUser.Name = "Mike";
+            testUser.Email = "mike@gmail.com";
+            testUser.CreationDate = DateTime.Parse("2018 - 08 - 10");
+            testUser.Id = 1;
+            user.Add(testUser);
+            return user;
+        }
+        public List<User> GetNotAssignedUsers(int id)
+        {
+            List<User> user = new List<User>();
+            User testUser = new User();
+            testUser.Name = "Mike";
+            testUser.Email = "mike@gmail.com";
+            testUser.CreationDate = DateTime.Parse("2018 - 08 - 10");
+            testUser.Id = 1;
+            user.Add(testUser);
+            return user;
+        }
+
+
+
+
+    }
+}
